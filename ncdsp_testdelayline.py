bf4c0d2e4ae2ad0a93655e931023be2c7ca22b75
Mark <mark@pepito.(none)>
2013-03-15 19:21:41 -0400

    added ncdsphelpers.py to split core code and extra functions

--- ncdsp_testdelayline.py
+++ ncdsp_testdelayline.py
@@ -6,6 +6,7 @@
 
 import sys
 
+from ncdsphelpers import *
 from ncdsp import *
 
 # create the delay line
---
+++ ncdsphelpers.py
@@ -0,0 +1,21 @@
+
+from ncdsp import *
+
+def createdelayline( name, numberofcells ):
+
+    for i in range(1,numberofcells):
+        cellname = '{0}_{1}'.format( name, i )
+        # take the value of the previous cell at the previous timestep
+        valfuncs[cellname] = eval( "lambda time: read( ('{0}_{1}',time-1) )".format(name,i-1) )
+
+    # cell 0 reads from name_input at the current time
+    valfuncs['{0}_0'.format(name)] = eval( "lambda time: read( ('{0}_input',time) )".format(name) )
+
+    # name_output reads from the last cell at the current time
+    valfuncs['{0}_output'.format(name)] = eval( "lambda time: read( ('{0}_{1}',time) )".format(name,numberofcells-1) )
+
+    # fill the delay line with zeroes at time zero, except for the first cell
+    # (so that it'll pull its value from name_input)
+    for i in range(1,numberofcells):
+        vals[( '{0}_{1}'.format(name,i), 0 )] = 0
+
