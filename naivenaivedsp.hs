e09f479c1ecfb9bb7f0881e003230ad6b9c07bc2
Mark Zadel <zadel@music.mcgill.ca>
2013-03-18 19:24:32 -0400

    commit

--- naivenaivedsp.hs
+++ naivenaivedsp.hs
@@ -1,4 +1,14 @@
 
+-- input: impulse at time n=0
 x 0 =  1.0
 x n =  0.0
+
+-- delay output by five samples
+y n = x $ n-5
+
+-- a list of xes and the corresponding ys
+xs = [ x n | n <- [0..40] ]
+ys = [ y n | n <- [0..40] ]
+
+main = print ys
 
