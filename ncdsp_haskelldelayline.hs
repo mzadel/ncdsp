88498877ac0146264d00786298153c2c5f1ebb5b
Mark Zadel <zadel@music.mcgill.ca>
2013-03-18 19:42:08 -0400

    commit

--- ncdsp_haskelldelayline.hs
+++ ncdsp_haskelldelayline.hs
@@ -17,7 +17,15 @@ y(n) = delay 4 (n-1)
 xs = [ x(n) | n <- [0..40] ]
 ys = [ y(n) | n <- [0..40] ]
 
+printdelay n = print [ delay 0 n, delay 1 n, delay 2 n, delay 3 n, delay 4 n ]
+
 main = do
+	-- print delay line contents at time 0
+	printdelay 0
+	-- print delay line contents at time 1
+	printdelay 1
+	-- print delay line contents at time 2
+	printdelay 2
 	print xs
 	print ys
 
