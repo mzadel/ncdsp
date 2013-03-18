a380d94b3920948a2d37dc4e9cdd5dfad81293af
Mark Zadel <zadel@music.mcgill.ca>
2013-03-18 19:25:56 -0400

    commit

--- naivenaivedsp.hs
+++ naivenaivedsp.hs
@@ -2,6 +2,7 @@
 -- input: impulse at time n=0
 x 0 =  1.0
 x n =  0.0
+-- nb: x 0 has to be first here -- it matches them in order
 
 -- delay output by five samples
 y n = x $ n-5
@@ -11,4 +12,6 @@ xs = [ x n | n <- [0..40] ]
 ys = [ y n | n <- [0..40] ]
 
 main = print ys
+
+-- this is actually quite clear...
 
