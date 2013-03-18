1139ea0d0830657a5fdd5df7c6e16c6992b6fd19
Mark Zadel <zadel@music.mcgill.ca>
2013-03-18 19:51:30 -0400

    commit

--- ncdsp_haskellpointerdelayline.hs
+++ ncdsp_haskellpointerdelayline.hs
@@ -8,4 +8,26 @@ x(n) = 0.0
 -- pointer circulating around the array
 pointer(n) = n `mod` 5
 
+delaylinecontents cell n
+	| cell == pointer(n) = x(n)
+	| otherwise = delaylinecontents cell (n-1)
+
+y(n) = delaylinecontents (pointer(n)) (n-1)
+
+-- a list of xes and the corresponding ys
+xs = [ x(n) | n <- [0..40] ]
+ys = [ y(n) | n <- [0..40] ]
+
+printdelay n = print [ delaylinecontents 0 n, delaylinecontents 1 n, delaylinecontents 2 n, delaylinecontents 3 n, delaylinecontents 4 n ]
+
+main = do
+	-- print delay line contents at time 0
+	printdelay 0
+	-- print delay line contents at time 1
+	printdelay 1
+	-- print delay line contents at time 2
+	printdelay 2
+	print xs
+	print ys
+
 
