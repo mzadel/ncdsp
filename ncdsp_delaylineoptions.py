fb65ec3e746c167010bcb04662f6a0164428fe4e
Mark Zadel <zadel@music.mcgill.ca>
2013-03-18 16:38:16 -0400

    commit

--- ncdsp_delaylineoptions.py
+++ ncdsp_delaylineoptions.py
@@ -18,9 +18,36 @@ def read( label ):
 valfuncs['x'] = lambda time: 1.0 if time==0 else 0.0
 valfuncs['y'] = lambda time: read(('x',time-M))
 
-l = [ read(('y',n)) for n in range(40) ]
-print l
+xs = [ read(('x',n)) for n in range(40) ]
+print xs
+ys = [ read(('y',n)) for n in range(40) ]
+print ys
 
+# then bucket brigade
+# then an implementation where the values stay in-place
+
+# -(bucket brigade)-----------------------------------------
+
+vals.clear()
+valfuncs.clear()
+
+valfuncs['x'] = lambda time: 1.0 if time==0 else 0.0
+
+valfuncs[('delay',0)] = lambda time: read(('x',time))
+valfuncs[('delay',1)] = lambda time: read((('delay',0),time-1))
+valfuncs[('delay',2)] = lambda time: read((('delay',1),time-1))
+valfuncs[('delay',3)] = lambda time: read((('delay',2),time-1))
+valfuncs[('delay',4)] = lambda time: read((('delay',3),time-1))
+
+valfuncs['y'] = lambda time: read((('delay',4),time))
+
+xs = [ read(('x',n)) for n in range(40) ]
+print xs
+ys = [ read(('y',n)) for n in range(40) ]
+print ys
+
+# so with this setup, there's only a delay of 4, even with 5 cells of delay
+# either the read or the write have to introduce an extra tick of delay
 
 
 
